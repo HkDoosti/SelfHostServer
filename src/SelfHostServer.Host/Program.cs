6868777b1ce67bbba777927e702b301c49ef9ca3
h_doosti <dustihassan@yahoo.com>
2023-12-13 09:47:52 +0330

    initial

---
+++ src/SelfHostServer.Host/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+
+var config = new HttpSelfHostConfiguration("http://localhost:5009");
+
+config.Routes
+    .MapHttpRoute(
+    "Default", "api/{controler}/{id}",
+    new { id = RouteParameter.Optional });
+
+HttpSelfHostServer server = new HttpSelfHostServer(config);
+
+server.OpenAsync().Wait();
+
+Console.ReadLine();
